779210c72db5f4f34a3edbbd134e3c12ff2cec25
Sam Grill <sgrill@spartaglobal.com>
2018-11-06 10:42:51 +0000

    initial commit

---
+++ src/main/java/xml_parsing_movies/App.java
@@ -0,0 +1,13 @@
+package xml_parsing_movies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
